9092a12add882cd7521df351a38bee321f2e2a1b
Matthew Davis <matc.davis@gmail.com>
2022-08-04 20:31:34 +0000

    write a comment on how the process.argv works

--- timer1.js
+++ timer1.js
@@ -1,5 +1,5 @@
 // process.stdout.write('\x07') makes a beep
-//need to modify here as the arrays elements are strings
+//need to modify the input here as the process.argv sets the arrays elements as strings
 
 let input = process.argv.slice(2).map(x => Number(x));
 input = input.filter(x => !(Number.isNaN(x) || typeof x !== 'number' || x <= 0));
