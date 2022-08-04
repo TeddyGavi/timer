42d3412778c1db7f794f2b7a1ed0e063513d226c
Matthew Davis <matc.davis@gmail.com>
2022-08-04 16:16:57 +0000

    refactored code to have only one filter statement

--- timer1.js
+++ timer1.js
@@ -2,8 +2,7 @@
 //need to modify here as the arrays elements are strings
 
 let input = process.argv.slice(2).map(x => Number(x));
-input = input.filter(x => !Number.isNaN(x) || typeof x !== 'number');
-input = input.filter(x => !(x <= 0));
+input = input.filter(x => !(Number.isNaN(x) || typeof x !== 'number' || x <= 0));
 
 if (input === undefined || input.length === 0) {
   console.log('No Beeps required');
